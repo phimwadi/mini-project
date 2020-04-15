0d12f8cf5630f42ea80fb2f6bf38956c963de4aa
phimwadi <phimwadi0033@gmail.com>
2020-04-15 22:00:56 +0700

    commit log out

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Config from '../config/Config'
 
 const Homepage =() => {
+
+    const logout = e =>{
+        Config.auth().signOut();
+    }
+
     return (
         <div>
             Home page
+            <div>
+            <button onClick={logout}>Log Out </button>
         </div>
+        </div>
+        
     )
 }
 
