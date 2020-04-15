e2090d4917393e3e81eb1eec50d96e2ce63809c8
phimwadi <phimwadi0033@gmail.com>
2020-04-15 21:35:18 +0700

    commit homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import { auth } from 'firebase';
+import Config from './config/Config';
+import Homepage from './components/Homepage';
 
 class App extends Component{
+  constructor(props){
+    super(props)
+      this.state={
+        user: {}
+      }
+    }
+
+  componentDidMount(){
+    this.authListener()
+  }
+  
+
+  authListener() {
+    Config.auth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({user})
+      }
+      else{
+        this.setState({user:null})
+      }
+    })
+  }
+
+
   render(){
+    if(this.state.user == null)
+    {
     return(
       <div>
         <Login/>
         </div>
       
+    );
+    }
+    return(
+      <div>
+        <Homepage/>
+      </div>
     )
   }
 }
