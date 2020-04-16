8ef8af9a78f7ed053affb14b4659f92958bf1f7a
phimwadi <phimwadi0033@gmail.com>
2020-04-17 03:20:34 +0700

    commit home

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,21 +1,74 @@
 import React from 'react'
 import Config from '../config/Config'
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const Homepage =() => {
-
-    const logout = e =>{
-        Config.auth().signOut();
+class Homepage extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        collapse: false,
+        isWideEnough: false,
+      };
+      this.onClick = this.onClick.bind(this);
     }
-
-    return (
+  
+    onClick() {
+      this.setState({
+        collapse: !this.state.collapse,
+      });
+    }
+    Logout(){
+        this.setState({})
+    }
+  
+    render() {
+      return (
         <div>
-            Home page
-            <div>
-            <button onClick={logout}>Log Out </button>
-            </div>
+          <header>
+            <Router>
+              <MDBNavbar color="bg-primary" fixed="top" dark expand="md" scrolling transparent>
+                <MDBNavbarBrand href="/">
+                  <strong>Navbar</strong>
+                </MDBNavbarBrand>
+                {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+                <MDBCollapse isOpen={this.state.collapse} navbar>
+                  <MDBNavbarNav left>
+                    <MDBNavItem active>
+                      <MDBNavLink to="#">Home</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem>
+                      <MDBNavLink to="#">Link</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem>
+                      <MDBNavLink to="#">Profile</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem>
+                      <MDBNavLink to="#">Logout</MDBNavLink>
+                    </MDBNavItem>
+                  </MDBNavbarNav>
+                </MDBCollapse>
+              </MDBNavbar>
+            </Router>
+  
+            <MDBView src="https://mdbootstrap.com/img/Photos/Others/img%20(40).jpg">
+              <MDBMask overlay="purple-light" className="flex-center flex-column text-white text-center">
+                <h2>This Navbar is fixed</h2>
+                <h5>It will always stay visible on the top, even when you scroll down</h5>
+                <p>Navbar's background will switch from transparent to solid color while scrolling down</p><br />
+                <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+              </MDBMask>
+            </MDBView>
+          </header>
+  
+          <main>
+            <MDBContainer className="text-center my-5">
+              <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </MDBContainer>
+          </main>
         </div>
-        
-    )
-}
+      );
+    }
+  }
 
 export default Homepage;
